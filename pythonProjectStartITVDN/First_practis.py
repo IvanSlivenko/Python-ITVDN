e6af438fb2eaacddb8a922dee844a7413c534c34
IvanSlivenko <ivan8822@ulr.net>
2024-01-05 16:40:49 +0200

    05-01-2024-16-40

---
+++ pythonProjectStartITVDN/First_practis.py
@@ -0,0 +1,54 @@
+# name = 'Ivan'
+# print('Hello', 'Ivan', 'Slivenko')
+# print('Hello', name, 'Slivenko')
+# print("\n")
+# name = 'Anna'
+# print('Hello', 'Anna', 'Slivenko')
+# print('Hello', name, 'Slivenko')
+# print("\n")
+
+from keyword import kwlist
+for i in kwlist:
+    print(i)
+
+
+
+
+
+
+
+
+
+
+# # Корисні функції пр роботі з числами
+#
+#
+# #---------------------------
+# #Додаємо знаки після коми
+#
+# num = 1
+# numFloat = float(1)
+#
+# print("numFloat", numFloat)
+# #--------------------------
+# #-Позбавляемось (обрізаємо) знаків після коми
+# numInt = int(numFloat)
+# print("numInt", numInt)
+#
+#
+# #---------------------------
+# absolute_n = abs(-5)
+# print("absolute_n", absolute_n)
+# #--------------------------
+#
+# rounden_n1: int = round(4.2312121)
+# print("rounden_n1", rounden_n1)
+#
+# rounden_n2: int = round(4.2312121, 2)
+# print("rounden_n2", rounden_n2)
+#
+# #--------------------------------------------
+# #--Округляємо до вказаної кількісті знаків після коми
+# prise = 7.25545
+# prise2 = round(prise, 2)
+# print('prise2', prise2)
