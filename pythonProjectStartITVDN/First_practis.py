12281554e1d2322581c01b7f4abd4fe652c5a91f
IvanSlivenko <ivan8822@ulr.net>
2024-01-07 12:18:40 +0200

    07-01-2024-12-18

--- pythonProjectStartITVDN/First_practis.py
+++ pythonProjectStartITVDN/First_practis.py
@@ -13,15 +13,57 @@
 # else:
 #     print('На дворі холодно')
 # #------------------------------------------------
-animal = ''
-if animal == 'cat':
-    print('Meo')
-elif animal == 'dog':
-    print('Wof')
-elif animal == 'bee':
-    print('Dzzz')
-else:
-    print('I don`t  know this animal')
+# animal = input('Вкажіть назву тварини')
+#
+# print(animal)
+# if animal == 'cat':
+#     print('Meo')
+# elif animal == 'dog':
+#     print('Wof')
+# elif animal == 'bee':
+#     print('Dzzz')
+# else:
+#     print('I don`t  know this animal')
 #-------------------------------------------------
+#------------------------------------------------- if     elif    else
+# user_name = input('Введіть логін \n')
+# user_age = int(input('Введіть ваш вік \n'))
+#
+# if user_name == 'Mark':
+#     print('Hello Mark вітаю у вашому кабінеті')
+# elif user_age >= 21:
+#     print('Your age is more then 21')
+# elif user_name == 'Ivan':
+#     print('Hello Ivan вітаю у вашому кабінеті')
+# else:
+#     print(f'Hello {user_name} чи бажаєте зареєструватись ?')
+
+
+# ------------------------------------------------and    or
+# user_login = 'Ivan'
+# user_name = input('Введіть логін \n')
+# user_age = int(input('Введіть ваш вік \n'))
+#
+# if user_name == user_login and user_age >= 18:
+#     print(f'Hello {user_name} вітаю у вашому кабінеті')
+# elif user_age < 18:
+#     print(f'Hello {user_name} ваш вік не дозволяє вам зареєструватись ')
+# else:
+#     print(f'Користувача {user_name} не знайдено у списку користувачів ')
+
+# ---------------------------------------------------------------------------------- len
+user_name = input('Введіть логін \n')
+user_name_len = len(user_name)
+if user_name_len <= 10:
+    print(f'Welcome {user_name}')
+else:
+    print('Ви ввели ім\'я що більше 10 символів',)
+
+
+#-------------------------------------------------Тернарні оператори
+
+
+
+
 
 
