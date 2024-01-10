7abb4922cc60dba071c114c3a695372cbf0bcca0
IvanSlivenko <ivan8822@ulr.net>
2024-01-10 21:16:15 +0200

    10-01-2024-21-16

---
+++ pythonProjectStartITVDN/Work file.py
@@ -0,0 +1,23 @@
+listOun = [1,2,3,4,5,6]
+print(listOun)
+
+listTwo = []
+print(listTwo)
+
+listThry = list('amsterdam')
+print(listThry)
+
+listFour = list()
+print(listFour)
+
+listfive = ['Ivan', 'David', 'Robert', 'Viktor', 'Olexandr']
+print(listfive[4])
+print(len(listfive))
+print(listfive[len(listfive)-1])
+print(listfive[-1])
+print(id(listfive))
+
+print(listfive)
+#------------------------------------------------- segment list
+print(listfive[1::1])
+
