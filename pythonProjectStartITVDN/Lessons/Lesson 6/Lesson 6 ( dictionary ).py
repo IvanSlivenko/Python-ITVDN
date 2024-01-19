6007c3e9790a0005d59d6d483b88fe0451cf3db5
IvanSlivenko <ivan8822@ulr.net>
2024-01-19 21:34:43 +0200

    19-01-2023-21-34

---
+++ pythonProjectStartITVDN/Lessons/Lesson 6/Lesson 6 ( dictionary ).py
@@ -0,0 +1,243 @@
+# dictionary structure ^
+#
+# 'key' - string
+# 'value' - string
+
+# {'key' : 'value'}
+
+
+# example_dict = {
+# 'first_name':'Ivan',
+# 'last_name':'Slivenko'
+# }
+# print(example_dict['first_name']) #--------------- Отримуємо за ключем
+#
+#
+# print(example_dict.get('last_name')) #------------- Отримуємо методом get
+
+#-----------------------------------------------------Створення словника Вар 1
+print('---------------------------------------------------------')
+capital_cities ={
+    'Venesuela' : 'Caracas',
+    'Nicaragua' : 'Managua',
+    'Ukraine' : 'Kiev'
+}
+
+print(capital_cities)
+#-----------------------------------------------------Створення словника Вар 2
+print('-------------------------------------------------------')
+capital_cities_2 = dict([['Venesuela' , 'Caracas'],['Nicaragua' , 'Managua'],['Ukraine' , 'Kiev']])
+
+print(capital_cities_2)
+
+#-----------------------------------------------------Створення словника Вар 3
+print('-------------------------------------------------------')
+capital_cities_3 = dict(Venesuela='Caracas', Nicaragua = 'Managua', Ukraina = 'Kiev' )
+print(capital_cities_3)
+print(capital_cities_3['Ukraina'])
+
+#----------------------------------------------------------------------- Додаємо елементи до словника
+
+#----------------------------------------------------------------------- Вар 1
+print('--------------------------------------------------------')
+
+capital_cities_3['Canada'] = 'Ottava'
+capital_cities_3['Georgia'] = 'Tbilisi'
+capital_cities_3['Moldova'] = 'Kishinev'
+capital_cities_3['China'] = 'Beijing'
+
+print(capital_cities_3)
+
+print('---------------------------------------------------------')
+#------------------------------------------------------------------Створення порожнього словника
+#-----------------------------------------Вар 1
+capital_cities_4 = {}
+print(capital_cities_4)
+
+#----------------------------------------Вар 2
+capital_cities_5: dict = dict()
+print(capital_cities_5)
+#----------------------------------------
+print('---------------------------------------------------------')
+
+cities_year_fundation:dict = dict()
+cities_year_fundation['Kharkiv'] = 1654
+cities_year_fundation['Chuguiv'] = 1638
+print(cities_year_fundation)
+cities_year_fundation['Kharkiv'] = {'Year for fundation': 1654, 'Main rivers': ['Kharkiv', 'Lopan']}
+print('----------------------------------------------------------')
+print(cities_year_fundation)
+print('---------------------------------------------------------')
+
+
+#-------------------------------------------------- Видаляємо елемент словника del
+del cities_year_fundation['Chuguiv']
+print(cities_year_fundation)
+print('-----------------------------------------------------------')
+#----------------------------------------------------Перебираємо словники
+print(capital_cities_3)
+for i in capital_cities_3:
+    print(i)  # -------------------------Перебираємо ключі
+    print(capital_cities_3[i])#--------------------------- Перебираємо значення
+
+    print(i, capital_cities_3[i])
+    print('--------------')
+
+
+print('-----------------------------------------------------------')
+
+
+#----------------------------------------- Методи словників
+# x = dir(dict())
+# for i in x:
+#     print(i)
+#----------------------------------- Переглядаємо список методів словників
+
+#------------------------------------ Деструктуризація Словників------------------items()
+print(capital_cities_3)
+for country, capital in capital_cities_3.items():
+    print('-------------------------------------')
+    print(f'{country} : {capital}')
+
+#------------------------------------- Очищення словників ------------------------clear()
+print('----------------------------------------------------------------------------')
+print(capital_cities_3)
+# capital_cities_3.clear()
+print(capital_cities_3)
+print('-----------------------------------------------------------------------------')
+#------------------------------------- Видаляємо значення за ключем та повертаємо його -------- pop()
+
+del_el = capital_cities_3.pop('China')
+print(del_el)
+print(capital_cities_3)
+print('---------------------------------------')
+#------------------------------------ Видаляємо ключ та значення та повертаємо його ----------- popitem()
+print(capital_cities_3)
+del_el_two = capital_cities_3.popitem()
+print(del_el_two)
+print(capital_cities_3)
+print('----------------------------------------')
+#------------------------------------ Додаємо у кінець новий елемент словника -------------- update()
+brazil = {'Brazil': 'Brazilia'}
+capital_cities_3.update(brazil)
+print(capital_cities_3)
+print('----------------------------------------')
+#----------------------------------- Отримуємо список значень (без ключів) ----------------- values()
+print(capital_cities_3)
+values_capital = capital_cities_3.values()
+print(values_capital)
+print('----------------------------------------')
+#------------------------------------Отримуємо список ключів ------------------------ keys()
+print(capital_cities_3)
+keys_capital_3 = capital_cities_3.keys()
+print(keys_capital_3)
+print('---------------------------------------')
+#------------------------------------- Отримуємо значення за ключем -- або None------------- get()
+print(capital_cities_3)
+print(capital_cities_3.get('Brazil'))
+print('--------------------------------------------')
+
+#--------------------------------- Повертаємо значення за замовчуванням ----------- setdefault()
+print(capital_cities_3)
+capital_cities_3.setdefault('Romania', 'Bucharest')
+print(capital_cities_3)
+print('--------------------------------------------------------')
+#--------------------------Повертаємо новий словник з ключами та значеннями (одним для всіх)---------- fromkeys()
+print(capital_cities_3)
+days_name_list = ['Monday', 'Thusdey', 'Wensdey']
+empty_dictionary: dict = {}
+new_dict = empty_dictionary.fromkeys(days_name_list, '24')
+print(new_dict)
+print('------------------------------------------------')
+
+#-------------------- Скільки разів Ім'я зустрічається у списку --- Варіант 1
+classmates_name = ['Sergiy', 'Igor', 'Tanya', 'Sergiy', 'Lena']
+print('classmates_name', classmates_name)
+answer = {}
+for i in classmates_name:
+    print(i)
+    if i in answer.keys():
+        answer[i] += 1
+    else:
+        answer[i] = 1
+print('answer', answer)
+print('---------------------------------------------------')
+#-------------------- Скільки разів Ім'я зустрічається у списку --- Варіант 2
+answerTwo = {}
+for i in classmates_name:
+    answerTwo[i] = classmates_name.count(i)#-------------------- count()
+print('answerTwo', answerTwo)
+print('--------------------------------------------------')
+
+#-------------------- пройтися за словником та виявити всі значення, що мають парний ключ Вар 1
+data_dict = {1 :'one', 33 : 'any text', 3 : 3, 5 : 5, 6 : 'x', 9: 'end'}
+print('data_dict', data_dict)
+print('значення, у яких ключі - парні')
+for el in data_dict:
+    if el % 2 == 0:
+        print(data_dict[el])
+print('----------------------------------------------------------------')
+
+#-------------------- пройтися за словником та виявити всі значення, що мають парний ключ Вар 2
+for key, value in data_dict.items():
+    if key % 2 == 0:
+        print(f' key {key} : value {value}')
+print('--------------------------------------------------------------')
+#-------------------створюємо словник з різними ключами та однаковими значеннями Вар 1
+data_dict_two = {}.fromkeys([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],'text')
+print('data_dict_two', data_dict_two)
+print('-----------------------------')
+
+#-------------------створюємо словник з різними ключами та однаковими значеннями Вар 2
+data_dict_five = {}.fromkeys(range(10),'text')
+print('data_dict_five', data_dict_five)
+print('-----------------------------')
+#------------------ створимо новий словник на підставі  поточного
+data_dict_three = {}
+counter = 0
+for key, value in data_dict_two.items():
+                counter += 1
+                new_value =(f'{key}.{value}')
+                data_segment = {str(counter) : str(new_value)}
+                data_dict_three.update(data_segment)
+
+print('data_dict_two', data_dict_two)
+print('data_dict_three', data_dict_three)
+print('---------------------------------------------------------------------')
+#------------------------------------------ Видаляємо пари словника, якщо тип ключа - стрічка
+data_dict_six = {
+                1:'value',
+                 'key' : 123,
+                 2: 'value',
+                 'key2': 123
+                 }
+data_dict_seven: dict = data_dict_six.copy() #----------Копіюємо словник
+for k in data_dict_six.keys(): # --------Перебираємо старий словник по ключам
+    # print(type(k))
+    if type(k) == str:
+        del data_dict_seven[k] #---------рефакторим новий словник
+        # print(k)
+print('data_dict_six', data_dict_six)
+print('data_dict_seven', data_dict_seven)
+print('-------------------------------------------')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pythonProjectStartITVDN/Work file.py
+++ pythonProjectStartITVDN/Work file.py
@@ -1,193 +1,81 @@
-# dictionary structure ^
+#  ['a','a', 'c'] -------------------------------------------- Список
+# {'key0' : 'value', 'key1' : 'value1' , 'key2' : 'value2 ]----------------- Словник
+# {'a', 'b', 'c'} --------------------------------------------- Множина
+# ( )----------------- Кортеж
 #
-# 'key' - string
-# 'value' - string
+from turtle import clear
 
-# {'key' : 'value'}
-
-
-# example_dict = {
-# 'first_name':'Ivan',
-# 'last_name':'Slivenko'
-# }
-# print(example_dict['first_name']) #--------------- Отримуємо за ключем
-#
-#
-# print(example_dict.get('last_name')) #------------- Отримуємо методом get
-
-#-----------------------------------------------------Створення словника Вар 1
-print('---------------------------------------------------------')
-capital_cities ={
-    'Venesuela' : 'Caracas',
-    'Nicaragua' : 'Managua',
-    'Ukraine' : 'Kiev'
-}
-
-print(capital_cities)
-#-----------------------------------------------------Створення словника Вар 2
-print('-------------------------------------------------------')
-capital_cities_2 = dict([['Venesuela' , 'Caracas'],['Nicaragua' , 'Managua'],['Ukraine' , 'Kiev']])
-
-print(capital_cities_2)
-
-#-----------------------------------------------------Створення словника Вар 3
-print('-------------------------------------------------------')
-capital_cities_3 = dict(Venesuela='Caracas', Nicaragua = 'Managua', Ukraina = 'Kiev' )
-print(capital_cities_3)
-print(capital_cities_3['Ukraina'])
-
-#----------------------------------------------------------------------- Додаємо елементи до словника
-
-#----------------------------------------------------------------------- Вар 1
-print('--------------------------------------------------------')
-
-capital_cities_3['Canada'] = 'Ottava'
-capital_cities_3['Georgia'] = 'Tbilisi'
-capital_cities_3['Moldova'] = 'Kishinev'
-capital_cities_3['China'] = 'Beijing'
-
-print(capital_cities_3)
-
-print('---------------------------------------------------------')
-#------------------------------------------------------------------Створення порожнього словника
-#-----------------------------------------Вар 1
-capital_cities_4 = {}
-print(capital_cities_4)
-
-#----------------------------------------Вар 2
-capital_cities_5: dict = dict()
-print(capital_cities_5)
-#----------------------------------------
-print('---------------------------------------------------------')
-
-cities_year_fundation:dict = dict()
-cities_year_fundation['Kharkiv'] = 1654
-cities_year_fundation['Chuguiv'] = 1638
-print(cities_year_fundation)
-cities_year_fundation['Kharkiv'] = {'Year for fundation': 1654, 'Main rivers': ['Kharkiv', 'Lopan']}
-print('----------------------------------------------------------')
-print(cities_year_fundation)
-print('---------------------------------------------------------')
-
-
-#-------------------------------------------------- Видаляємо елемент словника
-del cities_year_fundation['Chuguiv']
-print(cities_year_fundation)
-print('-----------------------------------------------------------')
-#----------------------------------------------------Перебираємо словники
-print(capital_cities_3)
-for i in capital_cities_3:
-    print(i)  # -------------------------Перебираємо ключі
-    print(capital_cities_3[i])#--------------------------- Перебираємо значення
-
-    print(i, capital_cities_3[i])
-    print('--------------')
-
-
-print('-----------------------------------------------------------')
-
-
-#----------------------------------------- Методи словників
-# x = dir(dict())
-# for i in x:
-#     print(i)
-#----------------------------------- Переглядаємо список методів словників
-
-#------------------------------------ Деструктуризація Словників------------------items()
-print(capital_cities_3)
-for country, capital in capital_cities_3.items():
-    print('-------------------------------------')
-    print(f'{country} : {capital}')
-
-#------------------------------------- Очищення словників ------------------------clear()
-print('----------------------------------------------------------------------------')
-print(capital_cities_3)
-# capital_cities_3.clear()
-print(capital_cities_3)
-print('-----------------------------------------------------------------------------')
-#------------------------------------- Видаляємо значення за ключем та повертаємо його -------- pop()
-
-del_el = capital_cities_3.pop('China')
-print(del_el)
-print(capital_cities_3)
-print('---------------------------------------')
-#------------------------------------ Видаляємо ключ та значення та повертаємо його ----------- popitem()
-print(capital_cities_3)
-del_el_two = capital_cities_3.popitem()
-print(del_el_two)
-print(capital_cities_3)
+#-----------------------------------------Створюємо множину Вар 1
+some_set = {'a', 'b', 'c', 'd', 'c'}
+print(some_set) #------------------------- Елементи лише унікальні
 print('----------------------------------------')
-#------------------------------------ Додаємо у кінець новий елемент словника -------------- update()
-brazil = {'Brazil': 'Brazilia'}
-capital_cities_3.update(brazil)
-print(capital_cities_3)
-print('----------------------------------------')
-#----------------------------------- Отримуємо список значень (без ключів) ----------------- values()
-print(capital_cities_3)
-values_capital = capital_cities_3.values()
-print(values_capital)
-print('----------------------------------------')
-#------------------------------------Отрмуємо список ключів ------------------------ keys()
-print(capital_cities_3)
-keys_capital_3 = capital_cities_3.keys()
-print(keys_capital_3)
-print('---------------------------------------')
-#------------------------------------- Отримуємо значення за ключем -- або None------------- get()
-print(capital_cities_3)
-print(capital_cities_3.get('Brazil'))
+
+#-----------------------------------------Створюємо множину Вар 2
+some_set_two = set([1,2,3,4,5])
+print(some_set_two)
+print('------------------------------------------')
+#----------------------------------------------------------------
+some_set_three = set('develuper')
+print(some_set_three)# ---------------------будуть відібрані тільки інікальні значення та впорядковані
+list = list('develuper')
+print(list)
+print('-----------------------------------')
+#-------------------------------------- Визначаємо довжину множини len()
+some_set_four = set('devops')
+print(some_set_four)
+print(len(some_set_four))
+print('----------------------------------')
+
+# -------------------------------------- перевіряємо наявність символа в середині нашої колекції
+some_set_six = set('internet')
+print(some_set_six)
+print('w' in some_set_six)
+print('i' in some_set_six)
+print('-----------------------------------')
+#---------------------------------------- Методи множин
+# print(help(set)) # ------------------------------------------Перелік методів множин
+
+#-------------------------------------------------незмінна множина Frozenset
+some_frozen_set = frozenset('internet')
+# print(some_frozen_set)
+print('-----------------------------------')
+
+# print(dir(some_frozen_set))
+print('-----------------------------------------')
+#-------------------------------------------------- очищаємо множину
+print(some_set_six)
+some_set_six.clear()
+print(some_set_six)
+print('------------------------------------------')
+#---------------------------------------------------- видаляємо один (випадковий) елемент множини pop()
+print(some_set_four)
+some_set_four.pop()
+print(some_set_four)
+print('-------------------------------------------')
+#----------------------------------------------------- видаляємо один (конкретний) елемент з множини discard()
+#----------------------------------------------------- але не отримуємо помилки, якщо елементу немає у множині
+print(some_set_four)
+some_set_four.discard('e')
+print(some_set_four)
+some_set_four.discard('p')
+print(some_set_four)
 print('--------------------------------------------')
-
-#--------------------------------- Повертаємо значення за замовчуванням ----------- setdefault()
-print(capital_cities_3)
-capital_cities_3.setdefault('Romania', 'Bucharest')
-print(capital_cities_3)
-print('--------------------------------------------------------')
-#--------------------------Повертаємо новий словник з ключами та значеннями (одним для всіх)---------- fromkeys()
-print(capital_cities_3)
-days_name_list = ['Monday', 'Thusdey', 'Wensdey']
-empty_dictionary: dict = {}
-new_dict = empty_dictionary.fromkeys(days_name_list, '24')
-print(new_dict)
-print('------------------------------------------------')
-
-#-------------------- Скільки разів Ім'я зустрічається у списку --- Варіант 1
-classmates_name = ['Sergiy', 'Igor', 'Tanya', 'Sergiy', 'Lena']
-print('classmates_name', classmates_name)
-answer = {}
-for i in classmates_name:
-    print(i)
-    if i in answer.keys():
-        answer[i] += 1
-    else:
-        answer[i] = 1
-print('answer', answer)
-print('---------------------------------------------------')
-#-------------------- Скільки разів Ім'я зустрічається у списку --- Варіант 2
-answerTwo = {}
-for i in classmates_name:
-    answerTwo[i] = classmates_name.count(i)#-------------------- count()
-print('answerTwo', answerTwo)
-print('--------------------------------------------------')
-
-#-------------------- пройтися за словником та виявити всі значення, що мають парний ключ
-data_dict = {1 :'one', 33 : 'any text', 3 : 3, 5 : 5, 6 : 'x', 9: 'end'}
-print('data_dict', data_dict)
-print('значення, у яких ключі - парні')
-for el in data_dict:
-    if el % 2 == 0:
-        print(data_dict[el])
-print('----------------------------------------------------------------')
-
-
-
-
-
-
-
-
-
-
-
-
+#----------------------------------------------------- видаляємо один (конкретний) елемент з множини remove()
+#----------------------------------------------------- та отримуємо помилки, якщо елементу немає у множині
+print(some_set_four)
+some_set_four.remove('s')
+print(some_set_four)
+print('--------------------------------------------')
+# some_set_four.remove('z')
+print(some_set_four)
+print('--------------------------------------------')
+#-------------------------------------------------- додаємо елемент у множину на випадкову позицію add()
+print(some_set_four)
+some_set_four.add('z')
+print(some_set_four)
+some_set_four.add('w')
+print(some_set_four)
+print('---------------------------------------------')
+#-------------------------------------------------
 
 
