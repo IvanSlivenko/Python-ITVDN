1fb173f7753acc4a5336de52e9101d12d884b07f
IvanSlivenko <ivan8822@ulr.net>
2024-01-24 17:00:27 +0200

    24-01-2024-17-00

--- pythonProjectStartITVDN/Work file.py
+++ pythonProjectStartITVDN/Work file.py
@@ -21,7 +21,25 @@ example_3()
 print(' example_3 .........................')
 # print(help(example_3))
 
-def printing_positional_arguments(first, second):
-    print(f'{first=} {second=}')
-printing_positional_arguments('FIRST','SECOND')
 
+first, second, third, *anything_left = ('FIRST', 'SECOND', 'THIRD','FOURTH','FIFTH','...')
+print(first, second, third, anything_left)
+print('--------------------------------------')
+
+# def printing_positional_arguments(first, second,*anythining_left):
+def printing_positional_arguments(first, second, *test):
+    print(f'{first=} {second=}')
+printing_positional_arguments('FIRST','SECOND','THIRD','FOURTH','FIFTH','...')
+print('----------------------------------------')
+
+def print_sum_2(a, b,*elements):
+    return(a+b+sum(elements))
+
+result = print_sum_2(2,3,4,5,6,7,8,9,10)
+print('result:', result)
+
+def make_pizza(*topings):
+    for topping in topings:
+        print(f'- {topping}')
+make_pizza('mushrums','sausages','cheese','capers')
+print('-----------------------------------')
